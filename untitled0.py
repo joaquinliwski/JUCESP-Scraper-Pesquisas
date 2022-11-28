d05b2a7600b84ed896c49bc585a88a5f1aa374a2
Joaquín Liwski <68760480+joaquinliwski@users.noreply.github.com>
2022-11-28 17:06:09 -0300

    Final

--- untitled0.py
+++ untitled0.py
@@ -33,9 +33,9 @@ duration = 1000  # Set Duration To 1000 ms == 1 second
 #for year in list(range(2000,2023,1)):
 #    for month in list(range(1,13,1)):
 driver5 = webdriver.Chrome(executable_path = r'C:\Program Files\chromedriver.exe') #Execute driver
-months = range(1,13,1)
+months = [10]#range(8,13,1)
 for month in months:
-    year=2017
+    year=2022
     types1 = [ "1", "2"]
     types2 = ["6", "7", "8", "9", "10","3", "4", "5",
              "11", "12", "13", "14", "15", "16", "17", "18", "19",
